3e0f57c44e617f15de72055ea50ad2bac4c3a0ec
Jakub Husak <qba@qba-MacBookPro.local>
2020-05-31 17:50:16 +0200

    working basic xe

---
+++ various_flashers/M091_BASICXE41/makerom.sh
@@ -0,0 +1,29 @@
+I="BASIC XE 4.1.rom"
+O=basicxe_128.rom
+i=0
+z=""
+while [[ $i -lt 512 ]]; do 
+z+=FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+(( i = i + 1 ))
+done
+function out8k() {
+echo "$z" | xxd -r -ps
+}
+
+echo -n >$O
+dd iseek=1 if="$I" of=/dev/stdout bs=4096 count=1 >>$O
+dd iseek=0 if="$I" of=/dev/stdout bs=4096 count=1 >>$O
+
+# for t in 1 2 ; do out8k >>$O ; done
+
+
+dd iseek=3 if="$I" of=/dev/stdout bs=4096 count=1 >>$O
+dd iseek=0 if="$I" of=/dev/stdout bs=4096 count=1 >>$O
+for t in 2 3 4 5 6 7 8  ; do out8k >>$O ; done
+
+dd iseek=2 if="$I" of=/dev/stdout bs=4096 count=1 >>$O
+dd iseek=0 if="$I" of=/dev/stdout bs=4096 count=1 >>$O
+
+for t in a b c d e f  ; do out8k >>$O ; done
+
+
