6a26ad4eb0ce82b91ca73cb165b6237286b22615
Jakub Husak <jakub.husak@ffserwis.pl>
2024-01-01 18:38:19 +0100

    fixed checking (when OPTION pressed)

--- various_flashers/makeflashwrite.sh
+++ various_flashers/makeflashwrite.sh
@@ -1,8 +1,8 @@
 #oldpwd=$(pwd)
 [ "$1" ] && [ -f "$1/macroflashname.asx" ] && cd "$1"
 ! [ -f macroflashname.asx ] &&
-	echo "Please cd into any flasher catalog and run ../$0" &&
-	echo "or run $0 <progcatalog>" &&
+	echo >/dev/stderr "Please cd into any flasher catalog and run ../$0" &&
+	echo >/dev/stderr "or run $0 <progcatalog>" &&
 	exit 1
 noprocess=$2
 outfile=$(pwd)
@@ -10,9 +10,9 @@ catalog=$(dirname "$outfile")
 outfile=$(basename "$outfile")flasher
 cd "$dirname"
 
-mads -m:macroflashname.asx ../../lib/flashwrite.asx -o:"$outfile".xex || exit
+! mads -m:macroflashname.asx -t -l ../../lib/flashwrite.asx -o:"$outfile".xex && echo >/dev/stderr ERROR! && exit 1
 
-echo Created file "$outfile".xex
+echo >/dev/stderr Created file "$outfile".xex
 #cd "$oldpwd"
 
 ! [ "$noprocess" == noprocess ] && [ -f ../postprocess.sh ] && [ -x ../postprocess.sh ] && ../postprocess.sh "$outfile".xex
